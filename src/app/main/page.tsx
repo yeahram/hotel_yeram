5c06d257dacc4a49bf40b2f026273a2c42bce087
yr623 <yeahram623@gmail.com>
2023-06-25 21:18:17 +0900

    haeder flex

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,16 @@
 const Main = () => {
   return (
     <>
-      <p>Main</p>
+      <p className="bg-slate-500 text-slate-50">Main</p>
+      <div>
+        <ul className="flex flex-auto">
+          <li>menu1</li>
+          <li>menu2</li>
+          <li>menu3</li>
+          <li>menu4</li>
+          <li>menu5</li>
+        </ul>
+      </div>
     </>
   );
 };
